6b54901eee023b321f32823f139acb6330262d3d
gbr715 <gbr715@alumni.ku.dk>
2023-05-01 16:34:30 +0200

    ehr

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,65 @@
+
+from types import SimpleNamespace
+
+import numpy as np
 from scipy import optimize
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
+import pandas as pd 
+import matplotlib.pyplot as plt
 
-    Args:
-        c (float): costs
-        alpha (float): parameter
+class HouseholdSpecializationModelClass:
 
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
+    def __init__(self):
+        """ setup model """
 
-    """ 
-    
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
+        # a. create namespaces
+        par = self.par = SimpleNamespace()
+        sol = self.sol = SimpleNamespace()
 
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
-    
-    return result
+        # b. parameters
+        par.eta = 1
+        par.sigma = 2
+        par.b = 0.5
+
+        #Solution
+        sol.W = np.zeros(1)
+        sol.L =np.zeros(1)
+
+   
+    def calc_union_utility(self,W):
+        """ calculate utility """
+
+        par = self.par
+        sol = self.sol
+
+        # a. Optimal price
+        P = par.sigma/(par.sigma-1)*W
+
+        # a. Optimal production
+        L = min((par.sigma/(par.sigma-1)*W)**-par.sigma,1)
+
+        # a. Labor demand
+        
+
+        # store optimal labor value in solution namespace
+        self.sol.L[0] = L
+
+        return (W-par.b)*L**par.eta
+
+    def solve(self,do_print=False):
+        """ solve model continously """
+        #Objective function set to minus utility
+        obj = lambda x: - self.calc_union_utility(x[0])  
+        #Bounds for choice variables  
+        bounds = [(0,100)]
+        #Initial guess for the optimizer
+        guess = [10]
+        #Minimizing the objective function (maximize utility)
+        result = optimize.minimize(obj, guess, method='Nelder-Mead',bounds=bounds)
+        opt = SimpleNamespace()
+
+        opt.W = result.x[0]
+        opt.L = self.sol.L[0]
+
+
+        return opt
